5087a11ccc3e5adbe4fc8dcb7d6e8470bf196d17
EugeneChang <57244404+EugeneChangResola@users.noreply.github.com>
2021-04-07 15:41:09 +0800

    synalio prod campaign.html update css style

--- cypress/integration/examples/browser_tab.spec.js
+++ cypress/integration/examples/browser_tab.spec.js
@@ -1,5 +1,8 @@
 context('タブ切り替え',() =>{
   it('キー操作で切り替え',() => {
-    cy.visit('https://eugenechangresola.github.io/WebbotPages/PROD/SYNALIO/')
+    cy.visit('https://eugenechangresola.github.io/WebbotPages/PROD/SYNALIO/campaign.html')
+    //cy.get('#synalio-campaign',{timeout: 10000}).should('be.visible')
+    cy.get('iframe#synalio-iframe').should('exist')
+    cy.get('iframe#synalio-iframe').contains('会話を始める').should('exist')
   })
 })
